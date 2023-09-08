d0596eeec7d55a28ebffb2ee8bac525a055993b6
Darius Tyson <dariust@ksu.edu>
2023-09-08 16:13:06 -0500

    Add project files.

---
+++ GameProject1/Game1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameProject1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private InputManager _inputmanager;
+
+
+        private SpriteFont _playerControls;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _inputmanager = new InputManager();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            _playerControls = Content.Load<SpriteFont>("Controls");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                //Exit();
+
+            // TODO: Add your update logic here
+            _inputmanager.Update(gameTime);
+            if (_inputmanager.Exit) Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(_playerControls, "W A S D or the arrow keys to move. \n Press esc to quit", new Vector2(250, 10), Color.Black);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+
+
+        }
+    }
+}
